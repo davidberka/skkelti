ff7f349ce2a72a347bca314d16d48f8b3c1dca16
David Berka <davidberka2005@gmail.com>
2021-10-17 14:01:29 +0200

    dropdown-menu

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,24 @@ const slideNav = document.querySelector(".navigation");
 burgerMenu.addEventListener("click", () => {
   slideNav.classList.toggle("menu_active");
 });
+
+if (window.innerWidth < 1024) {
+  const navLinks = document.querySelectorAll(".nav__link > a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      // remove active className
+      const dropdownMenus = document.querySelectorAll(".dropdown_menu");
+      dropdownMenus.forEach((menu) => {
+        if (link.parentNode != menu.parentNode) {
+          menu.classList.remove("active");
+        }
+      });
+      // add active className to current dropdown
+      const dropdownMenu = link.nextElementSibling;
+      if (dropdownMenu) {
+        dropdownMenu.classList.toggle("active");
+      }
+    });
+  });
+}
